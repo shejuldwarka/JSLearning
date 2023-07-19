2a5079d21d2f79f59251db6d4c09cd19a076bb6f
shejuldwarka <dwarkashejul@gmail.com>
2023-07-19 23:26:27 +0530

    Program Run
    Program Run

---
+++ 01_variableAssign.js
@@ -0,0 +1,55 @@
+ var myFullName; //  variable Declaration
+ console.log(myFullName);
+
+ var firstLastName; //  variable Declaration
+ firstLastName = "Dwaraka Shejul"// variable Declaration and initiation
+ console.log(firstLastName);
+
+ var myFullName; // declaration
+ myFullName = "Dwaraka Ganesh Shejul" // variable Declaration and initiation
+ console.log(myFullName);
+ 
+ var myLove; // declaration
+myLove = "I love only JS"
+console.log(myLove);
+
+ 
+var firstName; // declaration
+firstName = "Dwarka" // Initiation
+console.log(firstName);
+
+
+var lastName; // declaration
+lastName = "Shejul" // Initiation
+console.log(lastName);
+
+
+var age; // declaration
+age  = "27" // Initiation
+console.log(age);
+
+
+var address; // declaration
+address = "Jalna" // Initiation
+console.log(address);
+
+
+var pinCode; // declaration
+pinCode = "431209" // Initiation
+console.log(pinCode);
+
+
+
+var gender; // declaration
+gender  = "female" // Initiation
+console.log(gender);
+
+
+var isMarried; // declaration
+isMarried = "false" // Initiation
+console.log(isMarried);
+
+
+
+
+
