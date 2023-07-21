6ce520052d05f84b13a15bb9c9c538b5be58b820
shejuldwarka <dwarkashejul@gmail.com>
2023-07-21 12:21:40 +0530

    run program2
    run program2

---
+++ 02_swapVariables.js
@@ -0,0 +1,29 @@
+  console.log('..................Swapping Using 2 value.......................');
+ var sweety = "Sweety";
+var cutie = "Cutie";
+console.log('Before Swap' , 'sweety -',sweety, 'cutie -', cutie);
+ var temp = sweety;
+ sweety = cutie;
+ cutie = temp;
+ 
+ console.log('After Swap', 'sweety-', sweety, 'cutie-', cutie);
+
+ console.log('=============================================================================');
+ console.log('.....................Swapping Using 3 value..............................');
+
+ var num1 = 100; // 200
+var num1 = 100; // 300
+var num1 = 100; // 100
+ console.log('Before Swap', 'num1-', num1=100, 'num2-', num2=200, 'num3-',num3=300 );
+var temp = num1;
+num1=num2;
+num2=num3;
+num3=temp;
+
+
+
+console.log('After Swapping', 'num1-',num1=200, 'num2-', num2=300,'num3-',num3=100);
+
+
+
+ 
---
+++ 02_typeOf.js
@@ -0,0 +1,53 @@
+var bankName; // Declaration
+console.log(bankName);
+
+
+var bankName; // Declaration
+bankName = " SBI Bank" // Initialization
+console.log(bankName);
+
+
+var mark; // Declaration
+mark = "string " // Initialization
+  console.log(mark)
+
+  var mark; // Declaration
+mark = " 90%" // Initialization
+console.log(mark);
+
+
+var dataType; // Declaration
+console.log(dataType);
+
+var dataType; // Declaration
+  console.log(dataType)
+ 
+
+  var mark; // Declaration
+mark = "true " // Initialization
+  console.log(mark)
+
+  var value; // Declaration
+  value = "boolean " // Initialization
+    console.log(value)
+   
+    var value; // Declaration
+    value = "Hundred and Seven " // Initialization
+      console.log(value)
+  
+      var mark; // Declaration
+mark = "string " // Initialization
+  console.log(mark)
+
+  var mark; // Declaration
+mark = "107" // Initialization
+  console.log(mark)
+
+  var mark; // Declaration
+mark = "Number" // Initialization
+  console.log(mark)
+
+
+ var once; //Declaration
+ once = " "
+ console.log(once);
